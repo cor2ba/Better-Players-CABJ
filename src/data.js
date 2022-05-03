bb8e3e168c341194fbb16b75495de4d24ab26864
cor2ba <cordobagabrielignacio@gmail.com>
2022-05-02 23:31:46 -0300

    Best Players C.A.B.J

--- src/data.js
+++ src/data.js
@@ -1,7 +1,14 @@
 export const Maradona = JSON.parse(`{"img":{"i":"https://tntsports.com.ar/__export/1635176794789/sites/tntsports/img/2021/10/25/maradonaboca.jpg"},"posicion":{"p":"Midfield"},"nombre":{"n":"Diego Maradona"},"club":{"ca":"Estrella Roja"}}`)
 export const Riquelme = JSON.parse(`{"img":{"i":"https://resizer.glanacion.com/resizer/H67wUjEugfOzT3hRawEOIlGxwZY=/1200x746/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/BPZX5263MZDBFNHZDVZ5BLP73Y.jpg"},"posicion":{"p":"Midfield"},"nombre":{"n":"Juan Roman Riquelme"},"club":{"ca":"Argentino Juniors"}}`)
 export const Palermo = JSON.parse(`{"img":{"i":"https://www.tiempofueguino.com/wp-content/uploads/2020/04/Mart%C3%ADn-Palermo2.jpg"},"posicion":{"p":"Forward"},"nombre":{"n":"Martin Palermo"},"club":{"ca":"Godoy Cruz"}}`)
+export const Tevez = JSON.parse(`{"img":{"i":"https://cdn.futbolperuano.com/sdi/2021/06/04/el-adios-del-apache-carlos-tevez-anuncia-su-salida-de-boca-juniors-923655.jpg"},"posicion":{"p":"Forward"},"nombre":{"n":"Carlos Tevez"},"club":{"ca":"Boca Juniors"}}`)
+export const Rojas = JSON.parse(`{"img":{"i":"https://images.ole.com.ar/2019/09/29/NQNbNzoj_600x338__1.jpg"},"posicion":{"p":"Forward"},"nombre":{"n":"ÁNGEL CLEMENTE ROJAS"},"club":{"ca":"Boca Juniors"}}`)
+export const Varallo = JSON.parse(`{"img":{"i":"https://img.vavel.com/rip-varallo-1-1-1590954669030.jpg"},"posicion":{"p":"Forward"},"nombre":{"n":"Francisco Varallo"},"club":{"ca":"Boca Juniors"}}`)
+export const Schelotto = JSON.parse(`{"img":{"i":"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt78d81ad283e166aa/615f3368dfb62209b2f46bf6/2bd462c46216b25692726df0ba18fc10e0a73c79.jpg?auto=webp&format=jpg&quality=100"},"posicion":{"p":"Forward"},"nombre":{"n":"Guillermo Barros Schelotto"},"club":{"ca":"Gimnasia"}}`)
+export const Bermúdez = JSON.parse(`{"img":{"i":"https://www.futbolred.com/files/article_main/uploads/2020/04/02/5e85810a58550.jpeg"},"posicion":{"p":"Centre Back"},"nombre":{"n":"Jorge Bermúdez"},"club":{"ca":"Deportes Quindío"}}`)
+export const Gatti = JSON.parse(`{"img":{"i":"https://static4.abc.es/media/deportes/2018/11/24/gatti-kOID--620x349@abc.jpg"},"posicion":{"p":"Goalkeeper"},"nombre":{"n":"Hugo Gatti"},"club":{"ca":"Atlanta"}}`)
+export const Rattin = JSON.parse(`{"img":{"i":"https://1.bp.blogspot.com/-r8YOpOB8GUI/XzcuO8bTr6I/AAAAAAAANOA/DQOwbrFrL5E8l3wHU_-E7qK52W4MBhWrwCLcBGAsYHQ/s1600/e437a7b74376ed83321cff0342de7060.jpg"},"posicion":{"p":"Midfield"},"nombre":{"n":"Antonio Rattin"},"club":{"ca":"Boca Juniors"}}`)
 
-var jugadores = [Maradona, Riquelme, Palermo];
+var jugadores = [Maradona, Riquelme, Palermo, Tevez, Rojas, Varallo, Schelotto, Bermúdez, Gatti, Rattin];
 
 export default jugadores;
